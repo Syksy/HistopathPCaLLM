50fa59461cc738b029393b6d70a91d6ab9350be5
T. D. Laajala <teemu.daniel.laajala@gmail.com>
2025-04-07 18:52:51 +0300

    Base Claude run script

---
+++ runClaude.py
@@ -0,0 +1,103 @@
+import anthropic
+import os
+
+import data
+import time
+
+# Environmental variables
+from dotenv import load_dotenv
+load_dotenv()
+
+# Working directory for project taken from env vars
+os.chdir(os.environ.get("ROOT_DIR") + "out\\")
+
+client = anthropic.Anthropic(
+    api_key=os.environ.get("ANTHROPIC_API_TOKEN"),
+)
+
+# Latest models taken from https://docs.anthropic.com/en/docs/resources/model-deprecations#model-status
+# Loop while there is still combinations left to run (i.e. overloaded error or similar, sleep it off)
+while True:
+    try:
+        for modelname in [
+            # Haiku
+            "claude-3-5-haiku-20241022",
+            # Sonnet(s)
+            "claude-3-5-sonnet-20240620",
+            "claude-3-5-sonnet-20241022",
+            "claude-3-7-sonnet-20250219",
+        ]:
+            # Prompts to iterate across
+            for promptIndex in data.getArrayPromptIndex():
+                # Input statements
+                for inputIndex in data.getArrayInputIndex():
+                    # Iterate across languages (0 = English, 1 = Finnish, ...)
+                    for lang in [0]:
+                        # Non-censored (value 0) or censored (1) version of the input statements
+                        for cens in [0, 1]:
+                            # Seed settings
+                            #for seed in [False, True]:
+                            for seed in [False]:
+                                # Temperature-parameter values
+                                for temperature in [0.0, 0.01, 0.1]:
+                                    # Run everything as triplicates
+                                    for rep in range(3):
+                                        # Construct file name
+                                        filename = ("HistopathPcaLLM_" + modelname
+                                                    + "_prompt" + str(promptIndex)
+                                                    + "_input" + str(inputIndex)
+                                                    + "_lang" + str(lang)
+                                                    + "_cens" + str(cens)
+                                                    + "_seed" + str(seed)
+                                                    + "_temp" + str(temperature)
+                                                    + "_rep" + str(rep)
+                                                    )
+                                        # Run only if the output file doesn't exist yet
+                                        if not os.path.isfile(os.path.realpath(filename + ".out")):
+                                            print("Running \n" + filename + "\n")
+                                            # Construct the prompt + statement query to send
+                                            query = data.getQuery(promptIndex, inputIndex, lang, cens)
+                                            print("With query: \n" + query + "\n")
+                                            startTime = time.time()
+                                            # Run the actual prompt itself
+                                            message = client.messages.create(
+                                                model=modelname,
+                                                max_tokens=5000,
+                                                temperature=temperature,
+                                                messages=[
+                                                    {
+                                                        "role": "user",
+                                                        "content": [
+                                                            {
+                                                                "type": "text",
+                                                                "text": query
+                                                            }
+                                                        ]
+                                                    }
+                                                ]
+                                            )
+                                            endTime = time.time()
+                                            response = "".join(message.content[0].text)
+                                            # Write output to a suitable file, output
+                                            f = open(filename + ".out", 'w', encoding="utf-8")
+                                            f.write(response)
+                                            f.close()
+                                            print("\nOutput:\n" + response)
+                                            # Record wall clock time for the API call (seconds with enough decimals)
+                                            f = open(filename + ".time", 'w', encoding="utf-8")
+                                            f.write(str(endTime - startTime))
+                                            f.close()
+                                            # Record the prompt put in (sanity checks etc)
+                                            f = open(filename + ".prompt", 'w', encoding="utf-8")
+                                            f.write(query)
+                                            f.close()
+                                            # Handle console output and sleep before next API call
+                                            print("\n\n")
+                                            time.sleep(3)  # Sleep a bit
+        break
+    except anthropic.InternalServerError as e:
+        print("Error: " + str(e))
+        time.sleep(120)
+    except ValueError as e:
+        print("Error: " + str(e))
+        time.sleep(120)
