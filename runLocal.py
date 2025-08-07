cc8cc4bdaefdff4346c9c8caf4924b8a3d79d758
Syksy <teemu.daniel.laajala@gmail.com>
2025-08-07 13:44:21 +0300

    Add GPT-OSS

--- runLocal.py
+++ runLocal.py
@@ -1,18 +1,21 @@
-from transformers import pipeline
+# LLM tools
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+import torch
+# HuggingFace deployment
 from huggingface_hub import login
 from huggingface_hub import snapshot_download
+# Generic imports
 import data
 import time
 import re
+import os
 import requests
+# Install notes
 # PyTorch installed with CUDA (GPU: RTX 5090, 32GB VRAM)
 # pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu129
 # Late July 2025
-import torch
-import os
 # For Qwen
 # pip install accelerate
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # API key load
 from dotenv import load_dotenv
@@ -35,7 +38,7 @@ except ImportError:
     torch.backends.cudnn.enabled = False
 
 # Download model snapshot(s) if needed
-if False:
+if True:
     # Login to HuggingFace to gain access to gated medgemma/gemmas/Qwen
     print("Logging in to HuggingFace...")
     login(token=os.environ.get("HUGGINGFACE_API_KEY"))
@@ -54,7 +57,9 @@ if False:
         "Qwen/Qwen3-30B-A3B",
         "Qwen/Qwen3-32B",
         "Qwen/Qwen3-235B-A22B-Thinking-2507",
-        "Qwen/Qwen3-235B-A22B-Instruct-2507"
+        "Qwen/Qwen3-235B-A22B-Instruct-2507",
+        "openai/gpt-oss-20b",
+        #"openai/gpt-oss-120b"
     ]:
         print("Downloading model snapshot for " + model)
         snapshot_download(
@@ -63,7 +68,7 @@ if False:
         )
         print("Model snapshot download completed")
 
-# Test running through (med)gemmas locally
+# Test running through (med)gemmas
 for modelname in [
     #"google/gemma-3-4b-it",
     #"google/gemma-3-12b-it",
@@ -77,7 +82,9 @@ for modelname in [
         task="text-generation",
         model=modelname,
         torch_dtype=torch.bfloat16,
+        #torch_dtype="auto",
         device="cuda",
+        #device_map="auto"
     )
     # Prompts to iterate across
     for promptIndex in data.getArrayPromptIndex():
@@ -120,6 +127,84 @@ for modelname in [
                                                 "content": [
                                                     {"type": "text", "text": query}
                                                 ]
+                                            }
+                                        ],
+                                    max_new_tokens=2048)
+                                    response = output[0]["generated_text"][-1]["content"]
+                                    endTime = time.time()
+                                    # Write output to a suitable file, output
+                                    f = open(filename + ".out", 'w', encoding="utf-8")
+                                    f.write(response)
+                                    f.close()
+                                    print("\nOutput:\n" + response)
+                                    # Record wall clock time for the API call (seconds with enough decimals)
+                                    f = open(filename + ".time", 'w', encoding="utf-8")
+                                    f.write(str(endTime - startTime))
+                                    f.close()
+                                    # Record the prompt put in (sanity checks etc)
+                                    f = open(filename + ".prompt", 'w', encoding="utf-8")
+                                    f.write(query)
+                                    f.close()
+                                    # Handle console output
+                                    print("\n\n")
+    # Clear GPU memory
+    del pipe
+    torch.cuda.empty_cache()
+
+# Test running through GPT-OSSes locally
+for modelname in [
+    "openai/gpt-oss-20b",
+    #"openai/gpt-oss-120b"
+]:
+    print("Creating pipeline...")
+    pipe = pipeline(
+        # "image-text-to-text",
+        task="text-generation",
+        model=modelname,
+        #torch_dtype=torch.bfloat16,
+        torch_dtype="auto",
+        #device="cuda",
+        device_map="auto"
+    )
+    # Prompts to iterate across
+    for promptIndex in data.getArrayPromptIndex():
+        # Input statements
+        for inputIndex in data.getArrayInputIndex():
+            # Iterate across languages (0 = English, 1 = Finnish, ...)
+            for lang in [0, 1]:
+                # Non-censored (value 0) or censored (1) version of the input statements
+                for cens in [0, 1]:
+                    # Seed settings
+                    # for seed in [False, True]:
+                    for seed in [False]:
+                        # Temperature-parameter values
+                        # for temperature in [0.0, 0.1]:
+                        for temperature in [0.0]:
+                            # Run everything as triplicates
+                            for rep in range(3):
+                                # Construct file name
+                                filename = ("HistopathPCaLLM_" + re.sub("/", "-", modelname)
+                                            + "_prompt" + str(promptIndex)
+                                            + "_input" + str(inputIndex)
+                                            + "_lang" + str(lang)
+                                            + "_cens" + str(cens)
+                                            + "_seed" + str(seed)
+                                            + "_temp" + str(temperature)
+                                            + "_rep" + str(rep)
+                                            )
+                                # Run only if the output file doesn't exist yet
+                                if not os.path.isfile(os.path.realpath(filename + ".out")):
+                                    print("Running \n" + filename + "\n")
+                                    # Construct the prompt + statement query to send
+                                    query = data.getQuery(promptIndex, inputIndex, lang, cens)
+                                    print("With query: \n" + query + "\n")
+                                    startTime = time.time()
+
+                                    output = pipe(text_inputs=
+                                        [
+                                            {
+                                                "role": "user",
+                                                "content": query
                                             }
                                         ],
                                     max_new_tokens=2048)
